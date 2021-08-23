4b01395a6aa7ec1b969741b7444d550b027c11a5
Bitl <bitlblox@gmail.com>
2021-08-23 13:32:33 -0700

    add configs to repo

--- scripts/batch/github_sync.bat
+++ scripts/batch/github_sync.bat
@@ -47,12 +47,43 @@ XCOPY "%cd%\Novetus\clients\2010L\content\fonts\libraries.rbxm" "%gamescriptdir%
 XCOPY "%cd%\Novetus\clients\2011E\content\fonts\libraries.rbxm" "%gamescriptdir%/2011E"
 XCOPY "%cd%\Novetus\clients\2011M\content\fonts\libraries.rbxm" "%gamescriptdir%/2011M"
 
-SET launcherscriptdir=%basedir%\launcher
-if not exist "%launcherscriptdir%" mkdir "%launcherscriptdir%"
-if not exist "%launcherscriptdir%/3DView" mkdir "%launcherscriptdir%/3DView"
+echo.
+echo Copying default client configurations...
+SET tempdir=%CD%\cfg-temp
+if not exist "%tempdir%" mkdir "%tempdir%"
+XCOPY Novetus\config\clients\*.xml %tempdir% /sy
+del /s /q "%tempdir%\GlobalSettings2_2007E.xml"
+del /s /q "%tempdir%\GlobalSettings2_2007E-Shaders.xml"
+del /s /q "%tempdir%\GlobalSettings_4_2009E.xml"
+del /s /q "%tempdir%\GlobalSettings_4_2009E-HD.xml"
+del /s /q "%tempdir%\GlobalSettings_4_2010L.xml"
+del /s /q "%tempdir%\GlobalSettings_4_2011E.xml"
+del /s /q "%tempdir%\GlobalSettings_4_2011M.xml"
+del /s /q "%tempdir%\GlobalSettings4_2006S.xml"
+del /s /q "%tempdir%\GlobalSettings4_2006S-Shaders.xml"
+del /s /q "%tempdir%\GlobalSettings4_2007M.xml"
+del /s /q "%tempdir%\GlobalSettings4_2007M-Shaders.xml"
+del /s /q "%tempdir%\GlobalSettings7_2008M.xml"
+
+XCOPY "%tempdir%\GlobalSettings2_2007E_default.xml" "%gamescriptdir%/2007E"
+XCOPY "%tempdir%\GlobalSettings2_2007E-Shaders_default.xml" "%gamescriptdir%/2007E-Shaders"
+XCOPY "%tempdir%\GlobalSettings_4_2009E_default.xml" "%gamescriptdir%/2009E"
+XCOPY "%tempdir%\GlobalSettings_4_2009E-HD_default.xml" "%gamescriptdir%/2009E-HD"
+XCOPY "%tempdir%\GlobalSettings_4_2010L_default.xml" "%gamescriptdir%/2010L"
+XCOPY "%tempdir%\GlobalSettings_4_2011E_default.xml" "%gamescriptdir%/2011E"
+XCOPY "%tempdir%\GlobalSettings_4_2011M_default.xml" "%gamescriptdir%/2011M"
+XCOPY "%tempdir%\GlobalSettings4_2006S_default.xml" "%gamescriptdir%/2006S"
+XCOPY "%tempdir%\GlobalSettings4_2006S-Shaders_default.xml" "%gamescriptdir%/2006S-Shaders"
+XCOPY "%tempdir%\GlobalSettings4_2007M_default.xml" "%gamescriptdir%/2007M"
+XCOPY "%tempdir%\GlobalSettings4_2007M-Shaders_default.xml" "%gamescriptdir%/2007M-Shaders"
+XCOPY "%tempdir%\GlobalSettings7_2008M_default.xml" "%gamescriptdir%/2008M"
+rmdir "%tempdir%" /s /q
 
 echo.
 echo Copying launcher scripts...
+SET launcherscriptdir=%basedir%\launcher
+if not exist "%launcherscriptdir%" mkdir "%launcherscriptdir%"
+if not exist "%launcherscriptdir%/3DView" mkdir "%launcherscriptdir%/3DView"
 XCOPY "%cd%\Novetus\bin\preview\content\scripts\CSView.lua" "%launcherscriptdir%/3DView"
 XCOPY "%cd%\Novetus\config\ContentProviders.xml" "%launcherscriptdir%"
 XCOPY "%cd%\Novetus\config\splashes.txt" "%launcherscriptdir%"
@@ -60,7 +91,7 @@ XCOPY "%cd%\Novetus\config\splashes-special.txt" "%launcherscriptdir%"
 XCOPY "%cd%\Novetus\config\names-special.txt" "%launcherscriptdir%"
 
 echo.
-echo Moving client scripts and libraries to GitHub folder...
+echo Moving client scripts, libraries, and configurations to GitHub folder...
 SET dest=G:\Projects\GitHub\Novetus\Novetus_src
 SET scriptsdir=%dest%\scripts
 if not exist "%scriptsdir%" mkdir "%scriptsdir%"
