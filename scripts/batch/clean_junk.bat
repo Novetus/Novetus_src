5c18a023d67dccaa84d446867f9ebd89209e0c34
Bitl <bitlblox@gmail.com>
2021-11-04 10:36:44 -0700

    seperate cleannjunk into batch file

---
+++ scripts/batch/clean_junk.bat
@@ -0,0 +1,110 @@
+@ECHO OFF
+del /S Novetus\*.pdb
+del /S Novetus\*.log
+
+del /s /q Novetus\clients\2007E\content\scripts\CSMPBoot.lua
+del /s /q Novetus\clients\2007E-Shaders\content\scripts\CSMPBoot.lua
+del /s /q Novetus\clients\2007M\content\scripts\CSMPBoot.lua
+del /s /q Novetus\clients\2007M-Shaders\content\scripts\CSMPBoot.lua
+del /s /q Novetus\clients\2006S\content\scripts\CSMPBoot.lua
+del /s /q Novetus\clients\2006S-Shaders\content\scripts\CSMPBoot.lua
+
+del /s /q Novetus\clients\2006S\ReShade.ini
+del /s /q Novetus\clients\2006S\OPENGL32.log
+del /s /q Novetus\clients\2006S\opengl32.dll
+del /s /q Novetus\clients\2006S\DefaultPreset.ini
+del /s /q Novetus\clients\2006S\content\temp.rbxl
+
+del /s /q Novetus\clients\2006S-Shaders\ReShade.ini
+del /s /q Novetus\clients\2006S-Shaders\OPENGL32.log
+del /s /q Novetus\clients\2006S-Shaders\opengl32.dll
+del /s /q Novetus\clients\2006S-Shaders\DefaultPreset.ini
+del /s /q Novetus\clients\2006S-Shaders\content\temp.rbxl
+
+del /s /q Novetus\clients\2007E\ReShade.ini
+del /s /q Novetus\clients\2007E\OPENGL32.log
+del /s /q Novetus\clients\2007E\opengl32.dll
+del /s /q Novetus\clients\2007E\DefaultPreset.ini
+del /s /q Novetus\clients\2007E\content\temp.rbxl
+
+del /s /q Novetus\clients\2007E-Shaders\ReShade.ini
+del /s /q Novetus\clients\2007E-Shaders\OPENGL32.log
+del /s /q Novetus\clients\2007E-Shaders\opengl32.dll
+del /s /q Novetus\clients\2007E-Shaders\DefaultPreset.ini
+del /s /q Novetus\clients\2007E-Shaders\content\temp.rbxl
+
+del /s /q Novetus\clients\2007M\ReShade.ini
+del /s /q Novetus\clients\2007M\OPENGL32.log
+del /s /q Novetus\clients\2007M\opengl32.dll
+del /s /q Novetus\clients\2007M\DefaultPreset.ini
+del /s /q Novetus\clients\2007M\content\temp.rbxl
+
+del /s /q Novetus\clients\2007M-Shaders\ReShade.ini
+del /s /q Novetus\clients\2007M-Shaders\OPENGL32.log
+del /s /q Novetus\clients\2007M-Shaders\opengl32.dll
+del /s /q Novetus\clients\2007M-Shaders\DefaultPreset.ini
+del /s /q Novetus\clients\2007M-Shaders\content\temp.rbxl
+
+del /s /q Novetus\clients\2008M\ReShade.ini
+del /s /q Novetus\clients\2008M\OPENGL32.log
+del /s /q Novetus\clients\2008M\opengl32.dll
+del /s /q Novetus\clients\2008M\DefaultPreset.ini
+del /s /q Novetus\clients\2008M\content\temp.rbxl
+
+del /s /q Novetus\clients\2009E\ReShade.ini
+del /s /q Novetus\clients\2009E\OPENGL32.log
+del /s /q Novetus\clients\2009E\opengl32.dll
+del /s /q Novetus\clients\2009E\DefaultPreset.ini
+del /s /q Novetus\clients\2009E\content\temp.rbxl
+
+del /s /q Novetus\clients\2009E-HD\ReShade.ini
+del /s /q Novetus\clients\2009E-HD\OPENGL32.log
+del /s /q Novetus\clients\2009E-HD\opengl32.dll
+del /s /q Novetus\clients\2009E-HD\DefaultPreset.ini
+del /s /q Novetus\clients\2009E-HD\content\temp.rbxl
+
+del /s /q Novetus\clients\2010L\ReShade.ini
+del /s /q Novetus\clients\2010L\OPENGL32.log
+del /s /q Novetus\clients\2010L\opengl32.dll
+del /s /q Novetus\clients\2010L\DefaultPreset.ini
+del /s /q Novetus\clients\2010L\content\temp.rbxl
+
+del /s /q Novetus\clients\2011E\ReShade.ini
+del /s /q Novetus\clients\2011E\OPENGL32.log
+del /s /q Novetus\clients\2011E\opengl32.dll
+del /s /q Novetus\clients\2011E\DefaultPreset.ini
+del /s /q Novetus\clients\2011E\content\temp.rbxl
+
+del /s /q Novetus\clients\2011M\ReShade.ini
+del /s /q Novetus\clients\2011M\OPENGL32.log
+del /s /q Novetus\clients\2011M\opengl32.dll
+del /s /q Novetus\clients\2011M\DefaultPreset.ini
+del /s /q Novetus\clients\2011M\content\temp.rbxl
+
+del /s /q Novetus\clients\ClientScriptTester\ReShade.ini
+del /s /q Novetus\clients\ClientScriptTester\OPENGL32.log
+del /s /q Novetus\clients\ClientScriptTester\opengl32.dll
+del /s /q Novetus\clients\ClientScriptTester\DefaultPreset.ini
+del /s /q Novetus\clients\ClientScriptTester\content\temp.rbxl
+
+del /s /q Novetus\config\servers.txt
+del /s /q Novetus\config\ports.txt
+del /s /q Novetus\config\ReShade.ini
+del /s /q Novetus\config\config.ini
+del /s /q Novetus\config\config_customization.ini
+
+del /s /q Novetus\config\clients\GlobalSettings2_2007E.xml
+del /s /q Novetus\config\clients\GlobalSettings2_2007E-Shaders.xml
+del /s /q Novetus\config\clients\GlobalSettings_4_2009E.xml
+del /s /q Novetus\config\clients\GlobalSettings_4_2009E-HD.xml
+del /s /q Novetus\config\clients\GlobalSettings_4_2010L.xml
+del /s /q Novetus\config\clients\GlobalSettings_4_2011E.xml
+del /s /q Novetus\config\clients\GlobalSettings_4_2011M.xml
+del /s /q Novetus\config\clients\GlobalSettings4_2006S.xml
+del /s /q Novetus\config\clients\GlobalSettings4_2006S-Shaders.xml
+del /s /q Novetus\config\clients\GlobalSettings4_2007M.xml
+del /s /q Novetus\config\clients\GlobalSettings4_2007M-Shaders.xml
+del /s /q Novetus\config\clients\GlobalSettings7_2008M.xml
+
+rmdir /s /q Novetus\maps\Custom
+rmdir /s /q Novetus\shareddata\assetcache
