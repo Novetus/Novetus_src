0935cc521f12bca88db2eefac11a7f646b2c2670
Bitl <bitlblox@gmail.com>
2022-09-22 12:22:28 -0700

    Update AssetFixer.cs

--- Novetus/NovetusLauncher/Forms/SDK/AssetFixer.cs
+++ Novetus/NovetusLauncher/Forms/SDK/AssetFixer.cs
@@ -202,7 +202,7 @@ public partial class AssetFixer : Form
                     {
                         //https://stackoverflow.com/questions/10576686/c-sharp-regex-pattern-to-extract-urls-from-given-string-not-full-html-urls-but
                         List<string> links = new List<string>();
-                        var linkParser = new Regex(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=;]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                        var linkParser = new Regex(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=;\[\]]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
                         foreach (Match m in linkParser.Matches(line))
                         {
                             string link = m.Value;
