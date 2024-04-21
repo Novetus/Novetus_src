683d779b94b7d52f011065ab1b0342d52af62798
Bitl <bitlblox@gmail.com>
2024-04-21 09:21:39 -0700

    update script files

--- defaultaddons/core/AddonLoader.lua
+++ defaultaddons/core/AddonLoader.lua
@@ -1,6 +1,6 @@
 -- put script names here
 
-Addons = {"Utils", "ShadersCompatibility", "ServerWhitelist", "URLSetup"}
+Addons = {"Utils", "ServerWhitelist", "URLSetup"}
 
 -- DONT EDIT ANYTHING ELSE BELOW
 
