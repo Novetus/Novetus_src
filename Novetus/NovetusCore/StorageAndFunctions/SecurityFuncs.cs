ca46a17a4181d15a8348798bacea239c095921cf
Bitl <bitlblox@gmail.com>
2026-01-16 19:12:32 -0700

    remove outdated decoder

--- Novetus/NovetusCore/StorageAndFunctions/SecurityFuncs.cs
+++ Novetus/NovetusCore/StorageAndFunctions/SecurityFuncs.cs
@@ -170,33 +170,6 @@ namespace Novetus.Core
 				RenameWindow(exe, type, clientname, mapname);
 			}
 		}
-
-		//https://www.c-sharpcorner.com/article/caesar-cipher-in-c-sharp/
-		private static char cipher(char ch, int key)
-		{
-			if (!char.IsLetter(ch))
-			{
-				return ch;
-			}
-
-			char d = char.IsUpper(ch) ? 'A' : 'a';
-			return (char)((((ch + key) - d) % 26) + d);
-		}
-
-		public static string Encipher(string input, int key)
-		{
-			string output = string.Empty;
-
-			foreach (char ch in input)
-				output += cipher(ch, key);
-
-			return output;
-		}
-
-		public static string Decipher(string input, int key)
-		{
-			return Encipher(input, 26 - key);
-		}
 	}
 	#endregion
 }
